a3d93f0ee2abfd66eb374bb26c7c0134ccbae83f
DiegoGarV <107023284+DiegoGarV@users.noreply.github.com>
2026-07-06 13:00:00 -0600

    feat(room): created room activator

---
+++ Assets/Scripts/World/RoomActivator.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class RoomActivator : MonoBehaviour
+{
+    [Header("Waves")]
+    [SerializeField] GameObject waveSpawner;
+
+    [Header("Door")]
+    [SerializeField] List<GameObject> doors;
+    //[SerializeField] Transform closeDoor;
+
+    bool isActivated = false;
+    bool isFinished = false;
+
+    //private void Update()
+    //{
+    //    CompleteWave();
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!isFinished)
+            {
+                ActivateRoom();
+            }
+        }
+    }
+
+    private void ActivateRoom()
+    {
+        isActivated = true;
+        if (waveSpawner != null)
+        {
+            waveSpawner.gameObject.SetActive(true);
+        }
+        foreach (GameObject door in doors)
+        {
+            if (door != null)
+            {
+                door.SetActive(true);
+            }
+        }
+    }
+
+    public void CompleteWave()
+    {
+        isFinished = true;
+        isActivated = false;
+        if (waveSpawner != null)
+        {
+            waveSpawner.gameObject.SetActive(false);
+        }
+        foreach (GameObject door in doors)
+        {
+            if (door != null)
+            {
+                door.SetActive(false);
+            }
+        }
+    }
+}
